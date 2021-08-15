69f4a0431fc776362140223931b6ab8bc0c5f1c6
Anglesson Araujo <anglesson@hotmail.com>
2021-08-15 14:27:10 -0300

    refactor: ajusts in version on dependences packages

--- src/infra/db/mongodb/account-repository.spec.ts
+++ src/infra/db/mongodb/account-repository.spec.ts
@@ -1,24 +1,22 @@
+import { AccountMongoRepository } from './account-repository'
 import { MongoHelper } from './helpers/mongo-helper'
 
 describe('Account Mongo Repository', () => {
-  const url = process.env.MONGODB_URL ? process.env.MONGODB_URL : ''
-
   beforeAll(async () => {
-    await MongoHelper.connect(url)
+    await MongoHelper.connect()
   })
 
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
 
-  test('Should return account on success', async () => {
+  test('Should return an account on success', async () => {
     const sut = new AccountMongoRepository()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email',
       password: 'any_password'
     })
-
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
