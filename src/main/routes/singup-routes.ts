038ece0304a4c8707d970b25e1ee5630c4be57a6
Anglesson Araujo <anglesson@hotmail.com>
2021-10-12 19:11:44 -0300

    feat: add ExpressRouteAdapter

--- src/main/routes/singup-routes.ts
+++ src/main/routes/singup-routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { makeSignUpController } from '../factories/signup'
 import { adaptRoute } from '../adpters/express-route-adapter'
 
-export default (router: Router): any => {
+export default (router: Router): void => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/signup', adaptRoute(makeSignUpController()))
 }
